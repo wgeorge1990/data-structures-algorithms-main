a2ad0e82e45010e9d1df8c4427b8843880ea5201
WILLIAM GEORGE <williameverettgeorge@icloud.com>
2025-01-27 08:13:05 -0500

    Build Custom Array class that implements:  - print(), insert(index), removeAt(index), indexOf(item)

---
+++ src/main/java/com/wgdevelopment/Array.java
@@ -0,0 +1,64 @@
+package com.wgdevelopment;
+
+public class Array {
+    private int[] items;
+    private int count;
+
+    public Array(int length) {
+        items = new int[length];
+    }
+
+    public void print(){
+        for (int i = 0; i < count; i++){
+            System.out.println(items[i]);
+        }
+    }
+
+    public void insert(int item){
+        // if array is full we need to resize
+        if (items.length == count){
+
+            // create new array ( twice the size )
+            int[] newItems = new int[count * 2];
+
+            // copy all the existing items
+            for (int i = 0; i < count; i++)
+                newItems[i] = items[i];
+
+            // set "items" to this new array
+            items = newItems;
+        }
+
+        // add new item at the end of current array
+        items[count] = item;
+        count++;
+    }
+
+    public void removeAt(int index){
+        // validate index
+        if (index < 0 || index >= count)
+            throw new IllegalArgumentException("Index out of bounds");
+
+        // shift items to the left to fill the hole
+        // [10,20,30]
+        // index: 1
+        // 1 <- 2
+        // 2 <- 3
+        for (int i = index; i < count; i++){
+            items[i] = items[i + 1];
+        }
+        count--;
+    }
+
+    public int indexOf(int item){
+        // runtime complexity O(n)
+        for (int i = 0; i < count; i++){
+            // if item found, return index
+            if (items[i] == item)
+                return i;
+        }
+        return -1;
+    };
+
+
+}
